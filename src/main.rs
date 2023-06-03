bf4f01f7c5589a88a6b186024a45d46031d5b9f4
Jimmy_Tempest <smartwattana@gmail.com>
2023-06-04 01:34:45 +0700

    feat: sqlx setup and health check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
+use dotenv::dotenv;
+use std::env;
+
 mod routes;
 mod structs;
 mod utils;
@@ -9,9 +12,19 @@ use routes::index::*;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let _rocket = rocket::build()
+    dotenv().ok();
+
+    let database_url =
+        env::var("POSTGRES_DATABASE_URI").expect("POSTGRES_DATABASE_URI must be set");
+
+    let pool = sqlx::PgPool::connect(&database_url)
+        .await
+        .expect("Failed to connect to database");
+
+    rocket::build()
         .mount("/", routes![get_index])
         .mount("/", routes![health_check])
+        .manage(pool)
         .launch()
         .await?;
 
--- src/routes/health.rs
+++ src/routes/health.rs
@@ -1,12 +1,32 @@
 // mod structs;
 use rocket::serde::json::Json;
+use rocket::State;
+
+use sqlx::{Pool, Postgres};
 
 use crate::structs::*;
 
 #[get("/health-check")]
-pub fn health_check() -> Json<common::ResponseType<health::HealthCheckResponse>> {
-    // get memory consumption in megabytes
-    let health_check_response = health::HealthCheckResponse::new(true, "0.0001".to_string());
+pub async fn health_check(
+    pool: &State<Pool<Postgres>>,
+) -> Json<common::ResponseType<health::HealthCheckResponse>> {
+    let database_connection = match pool.acquire().await {
+        Ok(_) => true,
+        Err(_) => false,
+    };
+
+    // calculate database response time
+    let start = std::time::Instant::now();
+
+    let _ = sqlx::query("SELECT 1")
+        .execute(pool.inner())
+        .await
+        .expect("Failed to execute query");
+
+    let database_response_time = start.elapsed().as_millis().to_string();
+
+    let health_check_response =
+        health::HealthCheckResponse::new(database_connection, database_response_time);
     let response = common::ResponseType::new(health_check_response, None, None);
 
     Json(response)
