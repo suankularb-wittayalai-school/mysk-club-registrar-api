258372d83ea3e6b0fc4e498d57ccf33b444805d2
Jimmy-Tempest <smartwattana@gmail.com>
2023-06-18 08:17:24 +0700

    fix: cors for production

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ async fn main() -> std::io::Result<()> {
         let cors = Cors::default()
             .allowed_origin("http://localhost:3000")
             .allowed_origin("http://localhost:8000")
-            .allowed_origin("https://club.mysk.school")
+            .allowed_origin("https://clubs.mysk.school")
             .allowed_methods(vec!["GET", "POST", "PATCH", "DELETE", "PUT"])
             .allowed_headers(vec![
                 header::CONTENT_TYPE,
